53f22c4caf9c45bdc20f170fc4e94be65c0874d2
Gabe Goodhart <ghart@us.ibm.com>
2025-05-15 10:06:45 -0600

    feat: Add "update mode" to the main wrapper python script
    Branch: AddUpdate
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- scripts/openwebui.py
+++ scripts/openwebui.py
@@ -5,48 +5,80 @@
 #     "beeai-sdk==0.0.22"
 # ]
 # ///
+"""
+This script will configure and launch Open WebUI via uv with the appropriate
+added dependencies
+"""
 
-import os
-import base64
-import uvicorn
-import random
+# Standard
 from pathlib import Path
+import argparse
+import base64
+import importlib.metadata
+import os
+import random
 
-KEY_FILE = Path.cwd() / ".webui_secret_key"
+# Third Party
+import uvicorn
 
-os.environ["FROM_INIT_PY"] = "true"
-os.environ['WEBUI_SECRET_KEY'] = "l*cals3cre7*"
-os.environ["WEBUI_AUTH"] = "False"
+def update():
+    """When running for an update, don't do anything except print out the
+    version of open-webui
+    """
+    print(f"Open WebUI version: {importlib.metadata.version('open-webui')}")
+
+def main():
+    """When running as the persistent server, run and stay alive"""
+    KEY_FILE = Path.cwd() / ".webui_secret_key"
+
+    os.environ["FROM_INIT_PY"] = "true"
+    os.environ['WEBUI_SECRET_KEY'] = "l*cals3cre7*"
+    os.environ["WEBUI_AUTH"] = "False"
 
 
-if os.getenv("WEBUI_SECRET_KEY") is None:
-    if not KEY_FILE.exists():
-        KEY_FILE.write_bytes(base64.b64encode(random.randbytes(12)))
-    os.environ["WEBUI_SECRET_KEY"] = KEY_FILE.read_text()
+    if os.getenv("WEBUI_SECRET_KEY") is None:
+        if not KEY_FILE.exists():
+            KEY_FILE.write_bytes(base64.b64encode(random.randbytes(12)))
+        os.environ["WEBUI_SECRET_KEY"] = KEY_FILE.read_text()
 
-if os.getenv("USE_CUDA_DOCKER", "false") == "true":
-    LD_LIBRARY_PATH = os.getenv("LD_LIBRARY_PATH", "").split(":")
-    os.environ["LD_LIBRARY_PATH"] = ":".join(
-        LD_LIBRARY_PATH
-        + [
-            "/usr/local/lib/python3.11/site-packages/torch/lib",
-            "/usr/local/lib/python3.11/site-packages/nvidia/cudnn/lib",
-        ]
+    if os.getenv("USE_CUDA_DOCKER", "false") == "true":
+        LD_LIBRARY_PATH = os.getenv("LD_LIBRARY_PATH", "").split(":")
+        os.environ["LD_LIBRARY_PATH"] = ":".join(
+            LD_LIBRARY_PATH
+            + [
+                "/usr/local/lib/python3.11/site-packages/torch/lib",
+                "/usr/local/lib/python3.11/site-packages/nvidia/cudnn/lib",
+            ]
+        )
+        try:
+            import torch
+
+            assert torch.cuda.is_available(), "CUDA not available"
+        except Exception as e:
+            os.environ["USE_CUDA_DOCKER"] = "false"
+            os.environ["LD_LIBRARY_PATH"] = ":".join(LD_LIBRARY_PATH)
+
+
+
+    import open_webui.main  # we need set environment variables before importing main
+
+    host: str = "0.0.0.0"
+    port: int = 8080
+    reload: bool = True
+
+    uvicorn.run(open_webui.main.app, host=host, port=port, forwarded_allow_ips="*")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(__doc__)
+    parser.add_argument(
+        "-U",
+        "--update",
+        action="store_true",
+        default=False,
+        help="Run in 'update mode'. Just prints the version of Open WebUI and exits",
     )
-    try:
-        import torch
-
-        assert torch.cuda.is_available(), "CUDA not available"
-    except Exception as e:
-        os.environ["USE_CUDA_DOCKER"] = "false"
-        os.environ["LD_LIBRARY_PATH"] = ":".join(LD_LIBRARY_PATH)
-
-
-
-import open_webui.main  # we need set environment variables before importing main
-
-host: str = "0.0.0.0"
-port: int = 8080
-reload: bool = True
-
-uvicorn.run(open_webui.main.app, host=host, port=port, forwarded_allow_ips="*")
+    args = parser.parse_args()
+    if args.update:
+        update()
+    else:
+        main()
